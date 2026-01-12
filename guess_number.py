f154482d60076423f62bbbb1396875546a5f1704
mariaritalustosa <mritalustosa.ad@gmail.com>
2026-01-12 13:09:27 -0300

    feat: validate numeric user input

--- guess_number.py
+++ guess_number.py
@@ -2,3 +2,10 @@ import random
 
 print("Welcome to the Guess the Number Game!")
 choice_number = input("Digite o número máximo do jogo: ")
+
+if choice_number.isdigit():
+    choice_number = int(choice_number)
+
+else:
+    print("Por favor, digite um número válido.")
+    quit()
